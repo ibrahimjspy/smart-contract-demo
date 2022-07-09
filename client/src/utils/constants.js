fca500827a219c1ea92dcf14ae9c5a3c2c45d4b5
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-09 12:53:46 +0500

    contract address

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
 import abi from "./Transactions.json";
-
+// WARN!! this dont share contract address with anyone and it might not work according
+// to your desire 
 export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
 export const contractABI = abi.abi;
