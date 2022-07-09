5157e46427bb58da70d46984697cfe2795efaa31
ibrahimjspy <ibrahimsajidjs@gmail.com>
2022-07-09 15:19:00 +0500

    Solidity get accounts implementation .

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 This is done on top of a react scaffold I have worked
 with in past ,
-I've added CRUD functionality with demo accounts as well 
-You access demo accounts code in ./client/src/utils
-You can access connection context code in ./client/src/context
+I've added CRUD functionality with demo accounts as well .
 
-Warning this project to run at its desired capacity needs more time and some api 
+You can access demo accounts code in ./client/src/utils .
+You can access connection context code in ./client/src/context .
+
+Warning for this project to run at its desired capacity needs more time and some api 
 token integration from you .
-However because of this task main focus on code , I've worked mostly on that .
+However, main focus of this task is code , I've worked mostly on that .
 
 You can refer to both client and web3 individual readme's for more details 
--- web3/contracts/Transactions.sol
+++ web3/contracts/Transactions.sol
@@ -5,10 +5,16 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Transactions {
+    //Number of tokens and addresses
     uint256 Count;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
   // account schema 
+    struct AccountStruct {
+      address token_address;
+      uint token_amount;
+    }
+    // transaction address 
     struct TransferStruct {
         address sender;
         address receiver;
@@ -17,20 +23,27 @@ contract Transactions {
         uint256 timestamp;
         string keyword;
     }
-
-    TransferStruct[] accounts; // storing transaction in an array with object types 
-
+    // transactions list 
+    TransferStruct[] transactions; // storing transaction in an array with object types 
+    // accounts list 
+    AccountStruct[] accounts;
+    // for now no mock data for accounts only for transactions which are very similar 
+    //Watn only for task representation 
+    function getAllAccounts() public view returns (uint256) {
+        return accounts;
+    }
+    // transaction functions 
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         Count += 1;
-        accounts.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
     function getAllTransactions() public view returns (TransferStruct[] memory) {
-        return accounts;
+        return transactions;
     }
-
+    // this function can be used to get accounts count as well 
     function getTransactionCount() public view returns (uint256) {
         return Count;
     }
